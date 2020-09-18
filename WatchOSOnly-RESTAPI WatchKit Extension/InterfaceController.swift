787b2a517692c4c31445c5ec6da294873ea49aec
Brian Moriguchi <brian@beckos.com>
2020-09-18 00:18:24 -0700

    Initial Commit

---
+++ WatchOSOnly-RESTAPI WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchOSOnly-RESTAPI WatchKit Extension
+//
+//  Created by Tatsuya Moriguchi on 9/18/20.
+//  Copyright © 2020 Tatsuya Moriguchi. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
